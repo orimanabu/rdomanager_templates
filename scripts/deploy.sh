edeb21862453748e6bcfef6c323017ddfd89b7c3
Manabu Ori <manabu.ori+github@gmail.com>
2016-02-10 17:15:54 +0900

    refactoring

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#./heat_log.sh &
-#pid=$!
-#sleep 5
-#time openstack overcloud deploy --templates --ntp-server 10.5.26.10 --control-scale 1 --compute-scale 1 --neutron-tunnel-types vxlan --neutron-network-type vxlan --libvirt-type kvm --neutron-public-interface eth1
-#kill -9 ${pid}
-
 export LANG=C
 . ~/stackrc
 echo "=> start date"
@@ -22,18 +16,24 @@ time openstack overcloud deploy \
 --block-storage-flavor control \
 --control-scale 1 \
 --compute-scale 1 \
---ceph-storage-scale 1 \
 --libvirt-type kvm \
 --neutron-network-vlan-ranges datacentre:800:900 \
 -e ~/templates/tht/environments/network-isolation.yaml \
 -e ~/templates/network-environment.yaml \
--e ~/templates/storage-environment.yaml \
 -e ~/templates/all.yaml
+
+## ceph
+#--ceph-storage-scale 1 \
+#-e ~/templates/storage-environment.yaml \
+
+## for vxlan
+#--neutron-tunnel-types vxlan \
+#--neutron-network-type vxlan \
+
+## debug
 #-e ~/templates/ceph_osd_setup.yaml \
 #-e ~/templates/swap_setup.yaml \
 #-e ~/templates/misc_setup.yaml
-#--neutron-tunnel-types vxlan \
-#--neutron-network-type vxlan \
 
 echo "=> end date"
 date
